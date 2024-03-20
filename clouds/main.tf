356b7ccfcdb7c2d1145e8d4cb91cfe16b22ab23e
Vladimir Leveryev <volodya.leveryev@gmail.com>
2024-03-20 11:15:54 +0900

    Исправить ошибки в Terraform

--- clouds/main.tf
+++ clouds/main.tf
@@ -6,6 +6,10 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "my_policy" {
