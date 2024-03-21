46521fe1d3c46bcc9d09169495e13e1b7a2dc2f1
Vladimir Leveryev <volodya.leveryev@gmail.com>
2024-03-21 10:15:25 +0900

    Исправить ошибки в Terraform

--- clouds/main.tf
+++ clouds/main.tf
@@ -4,105 +4,100 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Создаем бакет S3
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
+}
+
+# Отключаем блокировку публичного доступа к бакету S3
+resource "aws_s3_bucket_public_access_block" "my_public_access" {
+  bucket = aws_s3_bucket.my_bucket.id
   block_public_acls = false
   block_public_policy = false
   ignore_public_acls = false
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "my_policy" {
+# Правило доступа к бакету
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
   bucket = aws_s3_bucket.my_bucket.id
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = ["s3:GetObject", "s3:PutObject"]
-        Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
-      },
-    ]
+    Statement = [{
+      Effect = "Allow"
+      Principal = "*"
+      Action = ["s3:GetObject"]
+      Resource = "${aws_s3_bucket.my_bucket.arn}/*"
+    }]
   })
 }
 
-resource "aws_s3_bucket_website_configuration" "web" {
+# Настраиваем веб-сайт
+resource "aws_s3_bucket_website_configuration" "my_web_config" {
   bucket = aws_s3_bucket.my_bucket.bucket
-
   index_document {
     suffix = "index.html"
   }
 }
 
-resource "aws_s3_object" "index" {
+# Загружаем файл
+resource "aws_s3_object" "index_page" {
   bucket = aws_s3_bucket.my_bucket.bucket
   key = "index.html"
   source = "./index.html"
   content_type = "text/html"
 }
 
-# resource "aws_s3_object" "video" {
-  
-# }
-
+# Находим TLS-сертификат
 data "aws_acm_certificate" "kit_imi_sertificate" {
-  domain = "*.kit-imi.info" 
+  domain = "*.kit-imi.info"
 }
 
 resource "aws_cloudfront_distribution" "cdn" {
-   origin {
-     domain_name = aws_s3_bucket_website_configuration.web.website_endpoint
-     origin_id = "unical-${var.bucket_name}"
+  aliases = ["${var.domain_name}.kit-imi.info"]
 
-    custom_origin_config {
-      http_port = 80
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
-   }
-   enabled = true
-   default_root_object = "index.html"
-   restrictions {
-     geo_restriction {
+  default_cache_behavior {
+    # Caching optimized
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = "${var.bucket_name}"
+    viewer_protocol_policy = "allow-all"
+  }
+
+  default_root_object = "index.html"
+
+  enabled = true
+
+  origin {
+    domain_name = aws_s3_bucket.my_bucket.bucket_regional_domain_name
+    origin_id = "${var.bucket_name}"
+  }
+
+  restrictions {
+    geo_restriction {
+      locations = []
       restriction_type = "none"
-     }
-   }
-
-   aliases = ["${var.domain_name}.kit-imi.info"]
-   default_cache_behavior {
-     target_origin_id = "unical-${var.bucket_name}"
-     cached_methods = ["GET", "HEAD"]
-     viewer_protocol_policy = "redirect-to-https"
-     allowed_methods = ["GET","HEAD"]
-
-     forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-     }
-   }
+    }
+  }
 
   viewer_certificate {
     acm_certificate_arn = "${data.aws_acm_certificate.kit_imi_sertificate.arn}"
     ssl_support_method = "sni-only"
-    minimum_protocol_version = "TLSv1.1_2016"
   }
 }
 
-
-data "aws_route53_zone" "cloudfront_zone" {
-  name         = "kit-imi.info."
+# Находим зону с доменными именами
+data "aws_route53_zone" "my_dns_zone" {
+  name = "kit-imi.info."
 }
 
-
+# Создаем доменное имя
 resource "aws_route53_record" "cloudfront" {
-  zone_id = data.aws_route53_zone.cloudfront_zone.zone_id
-  name    = "${var.domain_name}"
-  type    = "CNAME"
-  ttl     = 5
-
-  records        = ["${aws_cloudfront_distribution.cdn.domain_name}"]
+  zone_id = data.aws_route53_zone.my_dns_zone.zone_id
+  name = "${var.domain_name}"
+  type = "CNAME"
+  ttl = 5
+  records = ["${aws_cloudfront_distribution.cdn.domain_name}"]
 }
